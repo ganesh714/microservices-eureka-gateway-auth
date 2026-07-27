28b74c35840c48baff2aa4c19ceec5b25ccbdcb2
ganesh714 <evvganesh1@gmail.com>
2026-07-27 20:25:40 +0530

    feat(auth-service): make userName unique in AppUser and create AppUserRepository

--- auth-service/src/main/java/com/virax/microservices/authservice/model/AppUser.java
+++ auth-service/src/main/java/com/virax/microservices/authservice/model/AppUser.java
@@ -1,5 +1,6 @@
 package com.virax.microservices.authservice.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class AppUser {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@Column(unique = true)
 	private String userName;
 	private String password;
 	
